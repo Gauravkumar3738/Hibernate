3437129cdabd2019fe02ed8c97eb82bf43ea5441
gauravkumar3738 <gauravkumar3738@gmail.com>
2019-06-05 05:38:21 -0700

    SpringCalc

--- SpringMvcMaven/WebContent/index.jsp
+++ SpringMvcMaven/WebContent/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,7 +7,13 @@
 <title>Insert title here</title>
 </head>
 <body>
-<a href="hello1.html">Click Here1</a>
-<a href="hello2.html">Click Here2</a>
+
+	<a href="hello1">Click Here1</a>
+	<a href="hello2">Click Here2</a>
+
+	<form action="/hello1">
+		<input type="submit" value="click">
+	</form>
+	<h1>hey hi ${Ramesh}</h1>
 </body>
 </html>
--- SpringMvcMaven/src/com/mypack/HelloWorldController.java
+++ SpringMvcMaven/src/com/mypack/HelloWorldController.java
@@ -1,12 +1,25 @@
 package com.mypack;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HelloWorldController {
+public class HelloWorldController{
 	
-	@RequestMapping
+	@RequestMapping("/hello1")
 	public ModelAndView helloworld1()
 	{
-		
+		String message="Hello Spring MVC";
+		return new ModelAndView("display", "Ramesh", message);
 	}
 
+	
+	@RequestMapping("/hello2")
+	public ModelAndView helloworld2()
+	{
+		String message="Hello Spring MVC";
+		return new ModelAndView("display", "suresh", message);
+	}
+	
 }
